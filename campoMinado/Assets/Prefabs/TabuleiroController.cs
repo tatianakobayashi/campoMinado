0345fa8624fa67af277d8b5795a1d24f4e1c6b8e
tatianakobayashi <tatianakobayashi@hotmail.com>
2023-01-17 21:27:48 -0400

    dia 17-01

--- campoMinado/Assets/Prefabs/TabuleiroController.cs
+++ campoMinado/Assets/Prefabs/TabuleiroController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TabuleiroController: MonoBehaviour
 {
+
+    public GameObject[][] campos;
     // Start is called before the first frame update
     void Start()
     {
